2fd374d146070aec360c7a6f82937e79d9224e00
Daizen <dikehara@gmail.com>
2025-04-22 11:34:17 +0900

    Initial State

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
-const getManagementClient = require('../helpers/auth0ManagementClient');
 
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -23,40 +22,5 @@ router.get('/dashboard', requiresAuth() ,async function (req, res, next) {
   });
 });
 
-router.post('/dashboard', requiresAuth() , async function (req, res, next) {
-
-  // get an instance of the ManagementClient
-  const management = getManagementClient();
-
-  // Get a list of Self Service Profiles with current 
-  const ssProfilesResponse = await management.selfServiceProfiles.getAll();
-
-  // Use the 1st profile in this sample
-  const ssoProfile = ssProfilesResponse.data[0];
-  
-  // build options to generate a ticket for connection creation
-  const requestParameters = {
-    id: ssoProfile.id
-  };
-
-  // Generate connection name automatically and set current Auth0 application as an enabled client
-  const bodyParameters = {
-      connection_config: {
-        name: `self-service-sso-${Date.now()}` 
-      },
-      enabled_clients: [process.env.CLIENT_ID]
-  };
-
-  // generate a self sertvice sso ticket
-  const ssoTicket = await management.selfServiceProfiles.createSsoTicket(
-    requestParameters,
-    bodyParameters
-  );
-
-  res.render('dashboard', {
-    title: 'Admin Dashboard',
-    ticketURL: ssoTicket.data.ticket
-  });
-});
 
 module.exports = router;
